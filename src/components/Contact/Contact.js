17da07bf7c600b1af56821ee9af3c0ca91c4a485
rodri-ribes <rodri_ribes@hotmail.com>
2022-07-14 18:47:07 -0300

    first commit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,7 +9,7 @@ function Contact() {
     return (
         <div className={style.Container}>
             <h1 className={style.Title} id="contact" data-aos="fade-right">CONTACT</h1>
-            <hr className={style.hrAbout} data-aos="fade-left" />
+            <hr className={style.hrContact} data-aos="fade-left" />
             <p className={style.Text} data-aos="fade">Do you have something to propose to me? Contact me!</p>
             <Form />
             <Redes />
